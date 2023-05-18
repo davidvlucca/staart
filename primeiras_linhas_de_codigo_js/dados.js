1529389bde0e12231f32285c9fec70ae20aad437
David Lucca <david.v.lucca@gmail.com>
2023-05-18 23:22:09 +0100

    commit dados.js

---
+++ primeiras_linhas_de_codigo_js/dados.js
@@ -0,0 +1,27 @@
+/*
+nome = david
+idade = 20
+
+inteiros
+2.000 -8
+
+reais (ex: peso, altura, saldo do banco, dinheiro)
+1.23 -2.5
+
+caracteres (textos. ex: nome)
+'leticia' 'olha que legal essa foto que eu tirei' 'ola mundo'
+
+logicos (resultados de perguntas de sim ou nao)
+true - verdade , false - falso
+1 = true / 0 = falso
+
+*/
+
+var nome = 'leticia';
+console.log(nome)
+
+var dinheiro = 2000
+console.log(dinheiro)
+
+var peso = 68.3
+console.log(peso)
