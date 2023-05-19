ed6a9125f961c7a49caf68a5195f65c0c74be048
David Lucca <david.v.lucca@gmail.com>
2023-05-19 18:34:46 +0100

    Update tipos_especificos.js

--- primeiras_linhas_de_codigo_js/tipos_especificos.js
+++ primeiras_linhas_de_codigo_js/tipos_especificos.js
@@ -3,3 +3,19 @@
 //Number - numero
 var idade = 18;
 var saldo = 123.23;
+
+//Infinito
+var resultado = 42 / 0;
+console.log(resultado)
+
+//NaN - Not a number - Nao eh um numero
+const resultado_2 = 42 / 'leticia';
+console.log(resultado_2)
+
+//String - texto
+var nome = "Leticia";
+var contrato = "Declaro ...";
+
+//Booleano - Bool - Logico
+var verdadeiro = true;
+var falso = false;
