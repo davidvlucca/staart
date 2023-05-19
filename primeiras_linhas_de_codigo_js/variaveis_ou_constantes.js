bc1ec5a25b3b391fb42243cf90ce4bd41aa712c5
David Lucca <david.v.lucca@gmail.com>
2023-05-19 19:59:52 +0100

    Create variaveis_ou_constantes.js

---
+++ primeiras_linhas_de_codigo_js/variaveis_ou_constantes.js
@@ -0,0 +1,35 @@
+//var ou variavel - responsavel por armazenar um valor/informacao que voce pode usar depois no programa (e pode ser alterado)
+
+
+/*var nome_do_site = "Vendinha da Leticia";
+console.log(nome_do_site);
+
+nome_do_site = "Venda da Leticia"
+console.log(nome_do_site);
+
+var saldo = 100;
+saldo = saldo - 10;
+console.log(saldo);*/
+
+
+//const ou constante - variavel com a palavra "const" nao podem ter seus valores alterados posteriormente durante a execucao do programa
+
+const nome_do_site = "Vendinha da Leticia";
+console.log(nome_do_site);
+
+//nome_do_site = "Venda da Leticia"
+console.log(nome_do_site);
+
+//var => const, let
+
+//typeof - quando queremos saber o tipo de valor que alguma variavel armazena
+
+const variavel_1 = 3; // Number
+const variavel_2 = "3"; // String
+const variavel_3 = true; // Bool
+const variavel_4 = "true"; // String
+
+console.log(typeof variavel_1)
+console.log(typeof variavel_2)
+console.log(typeof variavel_3)
+console.log(typeof variavel_4)
