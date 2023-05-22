e49a32eb1c3561ecc50bd3d4c87fbb2012cad649
David Lucca <david.v.lucca@gmail.com>
2023-05-22 22:41:35 +0100

    Create operadores.js

---
+++ primeiras_linhas_de_codigo_js/operadores.js
@@ -0,0 +1,16 @@
+//Operadores
+//Aritmeticos, atribuicao, comparacao, logico
+
+
+/*
+aritmeticos
+soma (+)
+subtracao (-)
+multiplicacao (*)
+divisao (/)
+*/
+
+console.log(2+3);
+console.log(2-3);
+console.log(2*3);
+console.log(2/3);
