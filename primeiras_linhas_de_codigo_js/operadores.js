95637c4ff21fda9a32792ad87db95184084dfc20
David Lucca <david.v.lucca@gmail.com>
2023-05-22 23:19:26 +0100

    update operadores

--- primeiras_linhas_de_codigo_js/operadores.js
+++ primeiras_linhas_de_codigo_js/operadores.js
@@ -14,3 +14,53 @@ console.log(2+3);
 console.log(2-3);
 console.log(2*3);
 console.log(2/3);
+
+/*
+Incremento (++)
+Decremento (--)
+*/
+
+let saldo = 30;
+saldo++;
+console.log(saldo);
+
+let saldo2 = 40;
+console.log(--saldo2);
+console.log(saldo2);
+
+/*
+Negacao (-)
+Adicao (+)
+*/
+
+let saldo3 = 100;
+console.log(saldo3);
+console.log(-saldo3);
+
+let idade = '18';
+console.log(typeof idade);
+
+idade = +'18';
+console.log(idade);
+console.log(typeof idade);
+
+//Atribuicao - Dar o valor para a variavel
+
+const idade2 = 18;
+
+/*
+
+x += y
+
+x -= y
+
+x *= y
+
+x /= y
+
+*/
+
+let saldo4 = 100;
+saldo4 = saldo4 - 10;
+saldo4 -= 10;
+console.log(saldo4);
