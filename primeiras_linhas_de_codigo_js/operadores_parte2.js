3f12459108aed566da80c4e05f5028029a2de989
David Lucca <david.v.lucca@gmail.com>
2023-05-25 02:58:40 +0100

    Update operadores_parte2.js

--- primeiras_linhas_de_codigo_js/operadores_parte2.js
+++ primeiras_linhas_de_codigo_js/operadores_parte2.js
@@ -36,3 +36,16 @@ const nome_do_site3 = "Luana";
 
 const sao_diferentes = nome_do_cartao3 != nome_do_site3;
 console.log(sao_diferentes);
+
+const saldo_do_banco = 100;
+const saque_do_banco = 200;
+
+const pode_sacar = saldo_do_banco >= saque_do_banco;
+console.log(pode_sacar)
+
+/*
+Operadores Logicos
+&& - E
+// - OU
+! - Negacao
+*/
